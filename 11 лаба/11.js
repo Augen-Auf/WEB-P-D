f889ebca63a060344e9722ba7f33912b8e739faa
StarkPopStar <StarkPopStar@users.noreply.github.com>
2019-05-12 16:03:43 +0500

    almost ready

--- 11 лаба/11.js
+++ 11 лаба/11.js
@@ -1,29 +1,56 @@
-function enter()
-{
-    document.getElementById("contact").addEventListener("keyup",function(event)
-    {
-        event.preventDefault();
-        if(event.keyCode === 13) {
-            var newContact = document.getElementById('contact').value;
-            document.getElementById("contacts").style.display = "block";
-            document.getElementById('contacts').innerHTML += ('<input type = "checkbox" id="check" onChange="checkBox()" style = "display:block">') + newContact + ('<img src ="delete.png" onClick = "del()" style = "display:block"></img><br>');
-            //newContact.value = "";
+var counter = 0;
+function enter(event) {
+    var newContact = document.getElementById("contact").value;
+    if (event.keyCode === 13) {
+        var takeInfo = $("#contact").val();
+        var regContact = /.{1,99}\s[0-9,+]{1,15}\s.[a-zA-Z1-9\-\._]+@[a-z1-9]+?(\.[a-z1-9]+){1,40}/;
+        var checkContact = regContact.test(takeInfo);
+        if (newContact == "") {
+            alert("Введите данные в текстовое поле!");
+            return;
         }
-    })
-}
-function checkContact(){
-    var newContact = document.getElementById('contact').value;
-            //var regContact = /([А-ЯЁ][а-яё]+[\s]|([0-9]+[/s])|[a-zA-Z1-9\-\._]+@[a-z1-9]+?(\.[a-z1-9]+))/;
-            //newContact = newContact.match(regContact);
-}
-function checkBox(){
-    if(document.getElementById(check).checked)
-    {
-        document.getElementById(check).style.visibility = "hidden";
+        if (checkContact == true) {
+            document.getElementById("contacts").style.display = "block";
+            document.getElementById("contacts").innerHTML += '<li id = "changes' + counter + '" ondblclick="edit(' + "'" + 'changes' + counter + "')" + '"><input type = "checkbox" id="check' + counter +
+                '" onChange = "check(' + "'changes" + counter + "'" + ')">' + "<span id='edit" + counter + "'>" + newContact +
+                '</span><img src = "delete.png" onClick = "del(' + "'changes" + counter + "'" + ')">' + '<hr>' + '</li>';
+            counter++;
+        }
+        else {
+            alert("Данные введены некорректно!");
+        }
     }
-   
 }
-function del()
-{
-    document.getElementById("contacts").style.display = "none";
+function check(id) {
+    $('#' + id).css({ "color": "red" })
+    $('#' + id).css({ "text-decoration": "underline" });
+    var rePlace = id.replace("changes", "check");
+    $('#' + rePlace).css("visibility", "hidden");
+}
+function del(id) {
+    $('#' + id).css('display', 'none');
+}
+function edit(id) {
+    var rePlace = id.replace("changes", "edit");
+    var contact = $('#' + rePlace).text();
+    var newField = $('#' + id).html();
+    $('#' + id).html("<input type ='text' id='editContact'>");
+    $('#editContact').val(contact);
+    $('#editContact').focus();
+    $('#editContact').on("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            var info = $("#editContact").val();
+            var regContact = /.{1,99}\s[0-9,+]{1,15}\s.[a-zA-Z1-9\-\._]+@[a-z1-9]+?(\.[a-z1-9]+){1,40}/;
+            var agreeEdit = regContact.test(info);
+            if ((agreeEdit) == true) {
+                var editContact = $('#editContact').val();
+                $('#' + id).html(newField);
+                $('#' + rePlace).text(editContact);
+            }
+            else {
+                alert("Данные введены некорректно!");
+            }
+        }
+    });
 }
